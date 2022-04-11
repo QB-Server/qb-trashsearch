c6e0abcc47c5daaba9d5eb2d8cac60c27292f5ab
Bobbins <bobbins@protonmail.com>
2022-04-11 20:51:45 +0100

    Changed reward items

--- config.lua
+++ config.lua
@@ -19,13 +19,16 @@ Config.RewardTypes = {
 
 --rewards for small trashcans
 Config.RewardsSmall = {
-    [1] = {item = "cokebaggy", minAmount = 1, maxAmount = 3},
+    [1] = {item = "joint", minAmount = 1, maxAmount = 3},
     [2] = {item = "lockpick", minAmount = 1, maxAmount = 2},
-    [3] = {item = "vinremover", minAmount = 1, maxAmount = 1},
-    [4] = {item = "rolling_paper", minAmount = 1, maxAmount = 4},
-    [5] = {item = "plastic", minAmount = 1, maxAmount = 7},
-    [6] = {item = "harness", minAmount = 1, maxAmount = 1},
-    [7] = {item = "repairkit", minAmount = 1, maxAmount = 2},
+    [3] = {item = "beer", minAmount = 1, maxAmount = 1},
+    [4] = {item = "plastic", minAmount = 1, maxAmount = 4},
+    [5] = {item = "rubber", minAmount = 1, maxAmount = 7},
+    [6] = {item = "rolex", minAmount = 1, maxAmount = 1},
+    [7] = {item = "iron", minAmount = 1, maxAmount = 2},
+    [8] = {item = "empty_weed_bag", minAmount = 1, maxAmount = 3},
+    [9] = {item = "weed_og-kush", minAmount = 1, maxAmount = 2},
+    [10] = {item = "pistol_ammo", minAmount = 1, maxAmount = 2},
 }
 
 Config.Objects = {
